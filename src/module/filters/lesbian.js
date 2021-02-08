5133144a22a48959186281b03ad494210cf1c4f6
TheHandyUser <62173713+TheHandyUser@users.noreply.github.com>
2021-02-08 17:04:52 +0530

    pride flags

---
+++ src/module/filters/lesbian.js
@@ -0,0 +1,19 @@
+const Canvas = require(`canvas`);
+
+module.exports = class Lesbian {
+  /**
+   * Gay
+   * @param {image} image
+   */
+  async getImage(image) {
+    if (!image)
+      throw new Error(`You must provide an image as a first argument.`);
+    let bg = await Canvas.loadImage(`${__dirname}/../../assets/lesbian.png`);
+    let img = await Canvas.loadImage(image);
+    const canvas = Canvas.createCanvas(480, 480);
+    const ctx = canvas.getContext(`2d`);
+    ctx.drawImage(img, 0, 0, 480, 480);
+    ctx.drawImage(bg, 0, 0, 480, 480);
+    return canvas.toBuffer();
+  }
+};
